4b64d12d2cd8fa3a0b405b97d8ef948ecc24aa97
magas1507 <asmg15.07@gmail.com>
2026-05-10 21:07:18 -0300

    components controlled and uncontrolled

--- src/components/ControlledComponent/ControlledComponent.tsx
+++ src/components/ControlledComponent/ControlledComponent.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
+
 export const ControlledInput = () => {
+  const [value, setValue] = useState("")
   return (
     <div>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Ingresa el código del cupón. ej: 10OFF"
+        name=""
+        id=""
+      />
+      <p>Cupoón de descuento: <b>{value}</b></p>
     </div>
   );
 };
--- src/components/UncontrolledComponent/UncontrolledComponent.tsx
+++ src/components/UncontrolledComponent/UncontrolledComponent.tsx
@@ -1,6 +1,23 @@
+import { useRef } from "react";
+
 export const UncontrolledInput = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleSubmit = () => {
+    if (inputRef.current) {
+      alert(`Nuevo producto en el carrito: ${inputRef.current.value}`)
+    }
+  }
   return (
     <div>
+      <input
+        type="text"
+        ref={inputRef}
+        placeholder="Nombre del producto. Ej: manzana"
+        name=""
+        id=""
+      />
+      <button onClick={handleSubmit}>Añadir al carrito</button>
     </div>
   );
 };
